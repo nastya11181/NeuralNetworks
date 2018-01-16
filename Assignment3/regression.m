131b4c267379d0e93672bf2294c2c3755e6e9a2a
Nastya <Nastya>
2018-01-16 12:00:36 +0300

    assignment 2 continued

--- Assignment3/regression.m
+++ Assignment3/regression.m
@@ -4,11 +4,15 @@
 eta  = 0.05;
 
 % the max number of epochs
-tmax = 100;
+tmax = 200;
 
 % the number of examples we consider for training
-P = 100;
+P = 3000;
 
+% the testing data
+Q = 2000;
+
+% P+Q <= nlen = 5000
 
 % read in the data
 data = load("data3.mat");
@@ -27,35 +31,80 @@ w2 = rand(1, N);
 
 % make sure that the norm is 1
 w1 = w1/norm(w1);
-w2 = w2/norm(w2);
+ñcdw2 = w2/norm(w2);
 
 
-for t=1:tmax*P
-    % select training sample at random
-    x = randi(P);
-    x = xi(:, x);
-    t = tau(:, x);
+% create an array to keep track of the results
+errors = zeros(tmax, 3);
+
+for ep=1:tmax
     
-    % calculate sigma
-    s = sigma(x, w1, w2);
+    % create a random permutation of P training samples
+    perm = randperm(P);
+    E = 0;
     
-    % calculate quadratic deviation (contribution)
-    e = ((s-t)^2)/2;
+    % consider 1 sample at once
+    for p=1:P
     
-    % calculate delta
-    delta = (s-t)*...
+        % select training sample at random
+        ind = perm(p);
+        x = xi(:, ind);
+        t = tau(ind);
     
-    % calculate the gradients
+        % calculate sigma
+        s = sigma(x, w1, w2);
     
-    % update the weights
-    w1 = w1-eta*g1*e;
-    w2 = w2-eta*g2*e;
+        % calculate the gradients
+        g1 = (s-t)*(1-(tanh(w1*x))^2)*x;
+        g2 = (s-t)*(1-(tanh(w2*x))^2)*x;    
+    
+        % update the weights
+        w1 = w1-(eta*g1)';
+        w2 = w2-(eta*g2)';
+        
+        % calculate the error
+        E = E+(s-t)^2;
+    end
+    
+    % calculate the training error
+    E = 1/P*1/2*E;
+    
+    % save the results
+    errors(ep, 1) = ep;
+    errors(ep, 2) = E;
+    
+    
+    % calculate the test (generalization) error
+    Etest = 0;
+    for q=P+1:P+Q
+        x = xi(:, q);
+        t = tau(q);
+        
+        s = sigma(x, w1, w2);
+        
+        Etest = Etest+(s-t)^2;
+        
+    end
+    
+    Etest = 1/Q*1/2*Etest;
+    % save the results for plotting
+    errors(ep, 3) = Etest;
     
     
 end
 
-y = sigma(x, w1, w2)
+% display 
+errors
 
+% plot the cost function
+figure
+plot(errors(:, 1), errors(:,2))
+xlabel("Time in epochs, t")
+ylabel("Error, E")
+title("Cost function and the generalization (test) error function")
+hold on
+plot(errors(:,1), errors(:, 3))
+legend("cost function", "test error function")
 
 
 % function for calculating sigma
