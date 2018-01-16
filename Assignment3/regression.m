1233142b1247f4094b7a82107637734c045e3209
Nastya <Nastya>
2018-01-16 15:10:10 +0300

    assignment 3 finished

--- Assignment3/regression.m
+++ Assignment3/regression.m
@@ -1,16 +1,16 @@
-
+rng(1)
 
 %  the learning rate
 eta  = 0.05;
 
 % the max number of epochs
-tmax = 200;
+tmax = 500;
 
 % the number of examples we consider for training
-P = 3000;
+P = 4000;
 
 % the testing data
-Q = 2000;
+Q = 1000;
 
 % P+Q <= nlen = 5000
 
@@ -32,6 +32,22 @@ w2 = rand(1, N);
 % make sure that the norm is 1
 w1 = w1/norm(w1);
 w2 = w2/norm(w2);
+
+% calculate the test error before the training
+%{
+Etest = 0;
+for q=P+1:P+Q
+    x = xi(:, q);
+    t = tau(q);
+        
+    s = sigma(x, w1, w2);
+        
+    Etest = Etest+(s-t)^2;
+      
+end
+    
+Etest = 1/Q*1/2*Etest
+%}
 
 
 % create an array to keep track of the results
@@ -66,12 +82,14 @@ for ep=1:tmax
         E = E+(s-t)^2;
     end
     
-    % calculate the training error
+    % calculate the training error    
     E = 1/P*1/2*E;
     
     % save the results
     errors(ep, 1) = ep;
     errors(ep, 2) = E;
+
+    
     
     
     % calculate the test (generalization) error
@@ -89,22 +107,41 @@ for ep=1:tmax
     Etest = 1/Q*1/2*Etest;
     % save the results for plotting
     errors(ep, 3) = Etest;
-    
+   
     
 end
 
 % display 
 errors
 
-% plot the cost function
+% plot the cost function and the generalization error function
 figure
 plot(errors(:, 1), errors(:,2))
 xlabel("Time in epochs, t")
 ylabel("Error, E")
-title("Cost function and the generalization (test) error function")
+title("The cost function and the generalization (test) error function")
 hold on
 plot(errors(:,1), errors(:, 3))
 legend("cost function", "test error function")
+
+% plot the weights separately and on one plot
+figure
+subplot(1,2,1)
+bar(w1)
+title("Weight vector 1")
+
+subplot(1,2,2)
+bar(w2)
+title("Weight vector 2")
+
+
+figure
+bar(w1)
+hold on
+bar(w2)
+title("Complementation of the weight vectors")
+legend("W1", "W2")
+
 
 
 % function for calculating sigma
